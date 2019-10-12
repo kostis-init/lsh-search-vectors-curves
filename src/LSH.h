7372d9a87c258b71f62214df4202937e951d1798
Loukas Litsos <lkslts64@gmail.com>
2019-10-12 23:39:05 +0300

    Change LSH - with template to privde hasher
    HashTableStruct is now part of the LSH class.

--- src/LSH.h
+++ src/LSH.h
@@ -1,7 +1,9 @@
 #ifndef ALGORITHMS_PROJECT_LSH_H
 #define ALGORITHMS_PROJECT_LSH_H
 #include "Dataset.h"
+#include "HashTableStruct.h"
 
+template <typename T>
 class LSH {
 private:
     string inputFilename; bool inputFileGiven = false;
@@ -10,9 +12,12 @@ private:
     int numOfFunctions = 4;
     int numOfHashTables = 5;
 
-    DatasetPoints* data;
-    QueryDatasetPoints* queryData;
+    Dataset* data;
+    QueryDataset* queryData;
+    HashTableStruct<T> * tables;
+
 public:
+
     const string &getInputFilename() const;
 
     void setInputFilename(const string &inputFilename);
@@ -39,14 +44,94 @@ public:
 
     void setNumOfHashTables(int numOfHashTables);
 
-    DatasetPoints *getData() const;
+    Dataset *getDataset() const;
 
-    void setData(DatasetPoints *data);
+    void setData(Dataset *data);
 
-    QueryDatasetPoints *getQueryData() const;
+    QueryDataset *getQueryData() const;
 
-    void setQueryData(QueryDatasetPoints *queryData);
+    void setQueryData(QueryDataset *queryData);
+
+    void setHashTableStruct(HashTableStruct<T> *ht);
+
+    auto getHashTableStruct() { return tables;}
 };
 
+template <typename T>
+const string &LSH<T>::getInputFilename() const {
+    return inputFilename;
+}
+template <typename T>
+void LSH<T>::setInputFilename(const string &inputFilename) {
+    inputFileGiven = true;
+    this->inputFilename = inputFilename;
+}
+template <typename T>
+bool LSH<T>::isInputFileGiven() const {
+    return inputFileGiven;
+}
+template <typename T>
+const string &LSH<T>::getQueryFilename() const {
+    return queryFilename;
+}
+template <typename T>
+void LSH<T>::setQueryFilename(const string &queryFilename) {
+    queryFileGiven = true;
+    this->queryFilename = queryFilename;
+}
+template <typename T>
+bool LSH<T>::isQueryFileGiven() const {
+    return queryFileGiven;
+}
+template <typename T>
+const string &LSH<T>::getOutputFilename() const {
+    return outputFilename;
+}
+template <typename T>
+void LSH<T>::setOutputFilename(const string &outputFilename) {
+    outputFileGiven = true;
+    this->outputFilename = outputFilename;
+}
+template <typename T>
+bool LSH<T>::isOutputFileGiven() const {
+    return outputFileGiven;
+}
+template <typename T>
+int LSH<T>::getNumOfFunctions() const {
+    return numOfFunctions;
+}
+template <typename T>
+void LSH<T>::setNumOfFunctions(int numOfFunctions) {
+    this->numOfFunctions = numOfFunctions;
+}
+template <typename T>
+int LSH<T>::getNumOfHashTables() const {
+    return numOfHashTables;
+}
+template <typename T>
+void LSH<T>::setNumOfHashTables(int numOfHashTables) {
+    this->numOfHashTables = numOfHashTables;
+}
+template <typename T>
+Dataset *LSH<T>::getDataset() const {
+    return data;
+}
+template <typename T>
+void LSH<T>::setData(Dataset *data) {
+    this->data = data;
+}
+template <typename T>
+QueryDataset *LSH<T>::getQueryData() const {
+    return queryData;
+}
+template <typename T>
+void LSH<T>::setQueryData(QueryDataset *queryData) {
+    this->queryData = queryData;
+}
+
+template <typename T>
+void LSH<T>::setHashTableStruct(HashTableStruct<T> *ht) {
+    tables = ht;
+}
 
 #endif //ALGORITHMS_PROJECT_LSH_H
