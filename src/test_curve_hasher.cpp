8445a492fd791fc336c6d0b9e7f976dd5a8ae022
Loukas Litsos <lkslts64@gmail.com>
2019-10-14 19:43:32 +0300

    Add tests in curveHasher

--- src/test_curve_hasher.cpp
+++ src/test_curve_hasher.cpp
@@ -13,17 +13,32 @@ static FILE* temp_file = NULL;
 LSH<CurveHasher> *lsh;
 using namespace std;
 
+struct point_compare {
+   bool operator() (Point const point1,Point const point2) {
+    if (point1.coordinates.size() !=  point2.coordinates.size())
+        return true;
+    for (int i =0; i<point2.coordinates.size(); i++) 
+        if (point1.coordinates.at(i) == point2.coordinates.at(i))
+            continue;
+        else if (point1.coordinates.at(i) <point2.coordinates.at(i))
+            return true;
+        else 
+            return false;
+    return false;
+   }
+};
+
 void test_CurveHasher() {
-    auto chasher = new CurveHasher(2,10,10);
+    auto chasher = new CurveHasher(2,10,10,0);
     for (int i =0; i < 2; i++)
         CU_ASSERT(chasher->shiftedGrid[i] < 2.0);
-    chasher = new CurveHasher(7,10,10);
+    chasher = new CurveHasher(7,10,10,0);
     for (int i =0; i < 7; i++)
         CU_ASSERT(chasher->shiftedGrid[i] < 7.0);
 }
 
 void test_snap() {
-    auto chasher = new CurveHasher(2,3,3);    
+    auto chasher = new CurveHasher(2,3,3,0);    
     auto delta = 4 * 2 * 3 ;
     auto point = new Point("1");
     point->addCoordinateLast(90.4329482); point->addCoordinateLast(14.5342342);
@@ -60,40 +75,87 @@ void test_snap() {
     CU_ASSERT(snaped[0] != snaped2[0] && snaped[1] != snaped2[1]);
 }
 
-void test_Hash() {
-    int curveLen = 50;
-    int numCurves = 20;
-    vector<Point> pointVec;
+void test_Vectorize() {
+   //create a curve with length == 7.
+   //first three consecutive lie on the same grid curve so 
+   //only the first one should exist after removing duplicates.
+   //Same applies for points 4,5 and for points 6,7
+   vector<Point> pointVec;
+   pointVec.push_back(*(new Point(vector<double> {10.4324,100.4328})));
+   pointVec.push_back(*(new Point(vector<double> {11.89,98.489})));
+   pointVec.push_back(*(new Point(vector<double> {15.7668,96.603})));
+   pointVec.push_back(*(new Point(vector<double> {87.883,63.432})));
+   pointVec.push_back(*(new Point(vector<double> {88.883,62.432})));
+   pointVec.push_back(*(new Point(vector<double> {44.883,46.432})));
+   pointVec.push_back(*(new Point(vector<double> {49.883,43.432})));
+   auto curve = new Curve(pointVec);
+   //delta = 4 * 2 * 7 = 56.
+   auto chasher = new CurveHasher(2,7,7,0);
+   auto point = chasher->vectorize(chasher->snap(curve));
+   CU_ASSERT(point->getCoordinates().size() == 3);
+}
+
+vector<Curve> generateDataset(int numCurves,int curveLen,int lower,int upper) {
+    vector<Point> *pointVec;
+    pointVec = new vector<Point>[numCurves];
     vector<Curve> curveVec;
-    int lower = 0; 
-    int upper = 1000;
     uniform_real_distribution<double> dist(lower,upper);
     default_random_engine re;
-    for (int j=0; j < numCurves; j++) {
-        for (int i=0; i < curveLen; i++) 
-            pointVec.push_back(*(new Point(vector<double> (dist(re),dist(re)))));
-        curveVec.push_back(*(new Curve(pointVec)));
-    }
-    //delta 4 * 2 * 50 = 400
-    auto chasher = new CurveHasher(2,50,50);
-    size_t bucket;
-    set<size_t> buckets;
-    Point *prev,*curr = nullptr;
-    int sum =0;
     for (int i=0; i < numCurves; i++) {
-        //chasher->snap(&curveVec.at(i));
-        //bucket = (*chasher)(&curveVec.at(i));
-        curr = chasher->vectorize(chasher->snap(&curveVec.at(i)));
-        if (prev != nullptr)
-            if (*prev == *curr)
-                sum++;
-        //buckets.insert(bucket);
-        prev = curr;
+        for (int j=0; j < curveLen; j++) 
+            pointVec[i].push_back(*(new Point(vector<double> {dist(re),dist(re)})));
+        curveVec.push_back(*(new Curve(pointVec[i])));
     }
-    //printf("\nbucket last is %d\n",bucket);
-    //printf("bucket count is %d\n",buckets.size());
-    printf("sum of consecutive equal points is  %d\n",sum);
+    return curveVec;
 }
+
+void runHasher(int numCurves,int curveLen,int window,vector<Curve> curveVec,int *bucketRes,int *vectoriseRes) {
+    size_t bucket;
+    auto chasher = new CurveHasher(2,curveLen,curveLen,window);
+    set<size_t> buckets;
+    set<Point,point_compare> pointSet;
+    Point *prev = nullptr;
+    Point *curr = nullptr;
+    for (int i=0; i < numCurves; i++) {
+        auto point = chasher->vectorize(chasher->snap(&curveVec.at(i)));
+        pointSet.insert(*point);
+        bucket = (*chasher)(&curveVec.at(i));
+        buckets.insert(bucket);
+    }
+    *bucketRes = buckets.size();
+    *vectoriseRes = pointSet.size();
+}  
+
+void test_Hash() {
+   //test two curves that should lie on the same bucket.
+   vector<Point> _pointVec;
+   _pointVec.push_back(*(new Point(vector<double> (10.4324,100.4328))));
+   _pointVec.push_back(*(new Point(vector<double> (49.89,22.489))));
+   auto curve = new Curve(_pointVec);
+   vector<Point> _pointVec2;
+   _pointVec2.push_back(*(new Point(vector<double> (11.4324,98.2391))));
+   _pointVec2.push_back(*(new Point(vector<double> (48.777,20.777))));
+   auto curve2 = new Curve(_pointVec2);
+   //delta = 4 * 2 * 2 = 16
+   auto _chasher = new CurveHasher(2,2,2,20);
+   auto point = _chasher->vectorize(_chasher->snap(curve));
+   auto point2 = _chasher->vectorize(_chasher->snap(curve2));
+   CU_ASSERT(*point == *point2);
+   auto bucket = (*_chasher)(curve);
+   auto bucket2 = (*_chasher)(curve2);
+   CU_ASSERT(bucket == bucket2);
+   //test multiple curves (numCurves) with length = curveLen.
+   //the success of this test depends on window of PointHasher 
+   //that is created in CurveHashre constructor.
+
+   //delta = 4 * 2* 10 = 80
+   auto curveVec = generateDataset(3000,10,0,50);
+   int bucketRes, vectoriseRes;
+   int window = 20;
+   runHasher(3000,10,window,curveVec,&bucketRes,&vectoriseRes);
+   printf("results: points = %d, buckets = %d\n",vectoriseRes,bucketRes);
+}
+
 
 int init_suite1(void)
 {
@@ -127,7 +189,8 @@ int main(int argc,char *argv[]) {
    }
    if ((NULL == CU_add_test(pSuite, "test of CurveHasher constructor", test_CurveHasher))||
        (NULL == CU_add_test(pSuite, "test of round Coordinate", test_snap)) || 
-       (NULL == CU_add_test(pSuite,"test amplified hash func",test_Hash)))
+       (NULL == CU_add_test(pSuite,"test_Vectorize",test_Vectorize)) ||
+       (NULL == CU_add_test(pSuite,"test_Hash",test_Hash)))
       {
       CU_cleanup_registry();
       return CU_get_error();
